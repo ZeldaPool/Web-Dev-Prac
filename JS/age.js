77ca925ed2b11be94e5442fa85ac131bbbd806a8
ZeldaPool <smitrana007@gmail.com>
2019-02-09 10:38:21 +0530

    JS Basics

---
+++ JS/age.js
@@ -0,0 +1,7 @@
+var agey = prompt("What is your age in years");
+
+var aged;
+
+aged= agey*365;
+
+alert("Your Age in days is" +aged);
---
+++ JS/script.js
@@ -0,0 +1,6 @@
+var name = prompt("Whats your first name");
+var lastName = prompt("Whats your last name");
+var age = prompt("Whats your age");
+
+console.log("Your Name is " +name +" " +lastName);
+console.log("Your Age is "+age);

e939342a9c451d33d95d9d42bbedc95aa4934f1e
ZeldaPool <smitrana007@gmail.com>
2019-02-10 11:28:58 +0530

    NumGuessG

---
+++ JS/intjs.js
@@ -0,0 +1,21 @@
+var num= 11;
+
+var age = prompt("Enter A number between 1 to 20");
+
+
+
+if(age == Number(num))
+{
+	alert("YES THIS IS THE NAAAAMBEERR BINGPOT!");
+}
+
+else if(age> Number(num))
+{
+	alert("You entered too high");
+}
+
+else
+{
+	alert("You entered too low");
+}
+
